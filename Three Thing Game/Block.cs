a891e52eea3c964350c5b59556af1f2bb1d471bd
RoboChiz <RoboChiz@users.noreply.github.com>
2016-04-15 19:04:28 +0100

    Fixed the Block

---
+++ Three Thing Game/Block.cs
@@ -0,0 +1,18 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RobsSprite;
+#endregion
+namespace RobsPhysics
+{
+    public class Block : Sprite
+    {
+
+
+
+    }
+}
--- Three Thing Game/Three Thing Game/Block.cs
+++ Three Thing Game/Three Thing Game/Block.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using RobsPhysics;
 using RobsSprite;
 
@@ -9,7 +15,10 @@ namespace Three_Thing_Game
 {
     class Block : Sprite
     {
+         public Block(Texture2D textureVal, Vector2 pos, int widthVal, int heightVal) : base(textureVal,pos,widthVal,heightVal)
+         {
 
+         }
 
     }
 }
