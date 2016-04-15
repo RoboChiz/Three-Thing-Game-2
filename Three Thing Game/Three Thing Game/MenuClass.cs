3367bbfd5c67f90c99df7f54ffe3b98916890d36
iThorneZ <ad1996@hotmail.co.uk>
2016-04-15 19:50:11 +0100

    Menu class updated.

--- Three Thing Game/Three Thing Game/MenuClass.cs
+++ Three Thing Game/Three Thing Game/MenuClass.cs
@@ -9,11 +9,12 @@ using System.Text;
 
 namespace Three_Thing_Game
 {
-    class MenuClass : GameClass
+    class MenuClass : Game
     {
 
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Camera camera;
 
         private Sprite background, title, start, options, quit;
         private Texture2D backgroundT, titleT, startT, optionsT, quitT;
@@ -23,52 +24,99 @@ namespace Three_Thing_Game
         private int screenHeight = 600;
         private int sectX, sectY;
 
+        private int option;
+
         public MenuClass()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-        
+
         protected override void Initialize()
-        { 
+        {
+            camera = new Camera();
+
             base.Initialize();
         }
-        
+
+
         protected override void LoadContent()
         {
             sectX = screenWidth / 16;
             sectY = screenHeight / 16;
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            backgroundT = Content.Load<Texture2D>("blackSquare.png");
-            titleT = Content.Load<Texture2D>("blueSquare.png");
-            background = new Sprite(backgroundT, new Vector2(8*sectX, 0), screenWidth, screenWidth); //GameClass.  
-            title = new Sprite(backgroundT, new Vector2(0, 0), screenWidth, screenWidth);
-       
+            backgroundT = Content.Load<Texture2D>("blackSquare");
+            titleT = Content.Load<Texture2D>("blueSquare");
+            startT = Content.Load<Texture2D>("blueSquare");
+            optionsT = Content.Load<Texture2D>("blueSquare");
+            quitT = Content.Load<Texture2D>("blueSquare");
+
+            background = new Sprite(backgroundT, new Vector2(0, 0), screenWidth, screenHeight);
+            title = new Sprite(titleT, new Vector2(0, -2 * sectY), 4 * sectX, 2* sectY);
+            start = new Sprite(startT, new Vector2(0, 2 * sectY), 4 * sectX, (int)(1.5 * sectY));
+            options = new Sprite(optionsT, new Vector2(0, 4 * sectY), 4 * sectX, (int)(1.5 * sectY));
+            quit = new Sprite(quitT, new Vector2(0, 6 * sectY), 4 * sectX, (int)(1.5 * sectY));
         }
-        
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-       
+
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            if (GamePad.GetState(PlayerIndex.One).DPad.Up == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                option -= 1;
+                if (option == -1) option = 2;
+            }
+            else if (GamePad.GetState(PlayerIndex.One).DPad.Down == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                option += 1;
+                if (option == 2) option = 0;
+            }
+            else if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                if (option == 0)
+                {
+                    // TODO - start game
+                }
+                else if (option == 1)
+                {
+                    //TODO - OPTIONS 
+                }
+                else if (option == 2)
+                {
+                    Exit();
+                }
 
-            // TODO: Add your update logic here
-
+            }
             base.Update(gameTime);
         }
-       
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            var device = graphics.GraphicsDevice;
 
+            spriteBatch.Begin(SpriteSortMode.BackToFront,
+                      BlendState.AlphaBlend,
+                      null,
+                      null,
+                      null,
+                      null,
+                      camera.get_transformation(device));
+            title.Draw(spriteBatch);
+
+            start.Draw(spriteBatch);
+            options.Draw(spriteBatch);
+            quit.Draw(spriteBatch);
+
+            background.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
 
