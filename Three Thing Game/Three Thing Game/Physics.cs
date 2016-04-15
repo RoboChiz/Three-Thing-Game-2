4c5e6c3270ce9e6c35ac60291f65251207a9cfce
RoboChiz <RoboChiz@users.noreply.github.com>
2016-04-15 18:40:15 +0100

    Added Sprites

---
+++ Three Thing Game/Three Thing Game/Physics.cs
@@ -0,0 +1,156 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.GamerServices;
+using RobsSprite;
+#endregion
+
+namespace RobsPhysics
+{
+    class Physics
+    {
+
+        public static List<RigidBody> objs;
+
+        public Physics()
+        {
+            objs = new List<RigidBody>();
+        }
+
+        public class RigidBody : Sprite
+        {
+
+            public float Mass;
+            public float inv_mass;
+
+            public Vector2 Velocity;
+            public Vector2 Force;
+
+            public float terminalVelocity = 15f;
+
+            public bool colliding;
+            public RigidBody collidingWith;
+            public RigidBody avoid;
+
+            public RigidBody(Texture2D textureVal, Vector2 pos, int widthVal, int heightVal, float mass, float maxSpeed)
+                : base(textureVal, pos, widthVal, heightVal)
+            {
+                Mass = mass;
+
+                if (mass == 0)
+                    inv_mass = 0;
+                else
+                    inv_mass = 1 / mass;
+
+                Velocity = new Vector2(0, 0);
+                Force = new Vector2(0, 0);
+                terminalVelocity = maxSpeed;
+                colliding = false;
+
+                objs.Add(this);
+
+            }
+
+            public void AddForce(Vector2 dir)
+            {
+                Force += dir;
+            }
+
+            public bool checkCollision(RigidBody other)
+            {
+
+                Vector2 myCente = Position;
+                Vector2 thereCente = other.Position;
+
+                Vector2 normal = thereCente - myCente;
+
+                if (normal.Length() < Width / 2)
+                    return true;
+
+                return false;
+            }
+
+        }
+
+        void ResolveCollision(RigidBody A, RigidBody B)
+        {
+
+            A.Velocity = Vector2.Zero;
+            B.Velocity = Vector2.Zero;
+
+            // Calculate relative velocity
+            Vector2 rv = B.Velocity - A.Velocity;
+            Vector2 normal = B.Position - A.Position;           
+
+            // Calculate relative velocity in terms of the normal direction
+            float velAlongNormal = Vector2.Dot(rv, normal);
+
+            /*// Do not resolve if velocities are separating
+            if (velAlongNormal > 0)
+                return;*/
+
+            // Calculate restitution
+            float e = 0.1f;
+
+            // Calculate impulse scalar
+            float j = -(1 + e) * velAlongNormal;
+            j /= A.inv_mass + B.inv_mass;
+
+            // Apply impulse
+            Vector2 impulse = j * normal;
+            A.AddForce(-impulse);
+            B.AddForce(impulse);
+
+        }
+
+        public void Step(GameTime gameTime)
+        {
+
+            float deltaTime = (gameTime.ElapsedGameTime.Milliseconds / 1000f);
+
+            foreach (RigidBody rb in objs)
+            {
+
+                rb.colliding = false;
+
+                foreach (RigidBody rbo in objs)
+                {
+                    if (rbo != rb && (rb.avoid == null || rb.avoid != rbo) && rb.checkCollision(rbo))
+                    {
+                        rb.colliding = true;
+                        rb.collidingWith = rbo;
+                        ResolveCollision(rb, rbo);
+                    }
+                }
+
+                if (rb.Mass != 0)
+                {
+
+                    Vector2 currentVelocity = rb.Velocity;
+
+                    rb.Velocity += (rb.Force / rb.Mass) * deltaTime;
+
+                    if (rb.Velocity.Length() > rb.terminalVelocity)
+                        rb.Velocity = currentVelocity;
+
+                    rb.Force = new Vector2(0, 0);
+
+                    rb.Position += rb.Velocity * deltaTime;
+
+                }
+
+            }
+
+        }
+
+        public void AddObj(RigidBody rbody)
+        {
+            objs.Add(rbody);
+        }
+
+    }
+}
---
+++ Three Thing Game/Three Thing Game/Sprite.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RobsSprite
+{
+    public class Sprite
+    {
+        // Asset name of Sprite texture
+        public string assetName;
+
+        // Texture of the sprite
+        public Texture2D spriteTexture;
+
+        //The size of the Sprite (with scale applied)
+        public Rectangle Size;
+
+        public static float X = 0.0f;
+        public static float Y = 0.0f;
+
+        public int width;
+        public int height;
+
+        //The position of the Sprite
+        private Vector2 position = new Vector2(X, Y);
+
+        //The amount to increase/decrease the size of the original sprite. 
+        private float mScale = 1f;
+
+        // Rotation of sprite
+        public float rotation = 0f;
+
+        // Direction of sprite
+        private Vector2 direction;
+
+
+        private Color colour;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+         public float Scale
+        {
+            get { return mScale; }
+            set
+            {
+                mScale = value;
+                //Recalculate the Size of the Sprite with the new scale
+                Size = new Rectangle(0, 0, (int)(spriteTexture.Width * Scale), (int)(spriteTexture.Height * Scale));
+            }
+        }
+
+        public Vector2 GetCentre()
+         {
+             return Position + new Vector2(Width/2f, Height/2f);
+         }
+
+         public Color Colour
+         {
+             get { return colour; }
+             set { colour = value; }
+         }
+
+        public Sprite(Texture2D textureVal, Vector2 pos, int widthVal, int heightVal)
+        {
+            spriteTexture = textureVal;
+            Position = pos;
+            Width = widthVal;
+            Height = heightVal;
+            colour = Color.White;
+        }
+
+        public float FaceDirection(Vector2 dir)
+        {
+            rotation = (float)(Math.Atan2(-dir.Y, dir.X) + (85 * (Math.PI / 180)));
+            return rotation;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            int spriteWidth = (int)(Width);
+            int spriteHeight = (int)(Height);
+
+            int spriteX = (int)(Position.X);
+            int spriteY = (int)(Position.Y);
+
+            Rectangle destinationRectangle = new Rectangle(spriteX, spriteY, spriteWidth, spriteHeight);
+
+            Vector2 spriteOrigin = new Vector2(spriteTexture.Width / 2f, spriteTexture.Height / 2f);
+
+            spriteBatch.Draw(spriteTexture, destinationRectangle, null, Color.White, Rotation, spriteOrigin, SpriteEffects.None, 0);
+
+        }
+
+        public void DrawNoRot(SpriteBatch spriteBatch)
+        {
+            int spriteWidth = (int)(Width);
+            int spriteHeight = (int)(Height);
+            int spriteX = (int)(Position.X - (spriteWidth / 2f));
+            int spriteY = (int)(Position.Y - (spriteHeight / 2f));
+
+            spriteBatch.Draw(spriteTexture, new Rectangle(spriteX , spriteY, spriteWidth, spriteHeight), Colour);
+        }
+
+        public void DrawNoRotCentre(SpriteBatch spriteBatch)
+        {
+            int spriteWidth = (int)(Width);
+            int spriteHeight = (int)(Height);
+            int spriteX = (int)(Position.X);
+            int spriteY = (int)(Position.Y);
+
+            spriteBatch.Draw(spriteTexture, new Rectangle(spriteX, spriteY, spriteWidth, spriteHeight), Colour);
+        }
+
+        
+
+    }
+
+        public class Camera
+        {
+            public Vector2 position;
+            public float zoom = 1f;
+
+            public Camera(Vector2 pos, float zoomVal)
+            {
+                position = pos;
+                zoom = zoomVal;
+            }
+        }
+
+}
