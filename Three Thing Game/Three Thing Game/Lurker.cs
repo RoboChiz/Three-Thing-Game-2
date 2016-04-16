56b76bfef4059411e1f8c0f8f435d48d74154088
Andrew Gittus <A.B.Gittus@2014.hull.ac.uk>
2016-04-16 12:30:15 +0100

    Added Enemy - lurker which moves to a player's location

---
+++ Three Thing Game/Three Thing Game/Lurker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Text;
+using RobsPhysics;
+using RobsSprite;
+
+namespace Three_Thing_Game
+{
+    class Lurker : Enemy
+    {
+        Vector2 position;
+        Player player;
+        enum State
+        {
+            Idling,
+            Moving
+        }
+        public Lurker(Player _player): base(new Vector2(0.0f, 0.0f), 2, 2)
+        {
+            position = base.Position;
+            _player = player;
+        }
+
+        override public void EnemyAIUpdate()
+        {
+            float distanceFromPlayer = Vector2.Distance(position, player.Position);
+
+            if (distanceFromPlayer < 5.0f)
+            {
+                Vector2 dir = Vector2.Normalize(player.Position - this.Position);
+                Position += dir * 2f;
+            }
+        }
+
+    }
+}
